0583f61e07681325be777ec43b05ce5f0387fb92
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-04-14 16:54:07 -0300

    Finalizado Projeto DevMedia Métodos Construtores

---
+++ src/br/com/devmedia/construtores/Pessoa.java
@@ -0,0 +1,60 @@
+package br.com.devmedia.construtores;
+
+public class Pessoa {
+
+	private String nome;
+	private String sobrenome;
+	private int idade;
+	private char sexo;
+
+	public Pessoa() {
+		super();
+	}
+
+	public Pessoa(String nome, int idade) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+	}
+
+	public Pessoa(String nome, String sobrenome, int idade, char sexo) {
+		super();
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+		this.idade = idade;
+		this.sexo = sexo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+}
--- src/br/com/devmedia/construtores/Programa.java
+++ src/br/com/devmedia/construtores/Programa.java
@@ -3,7 +3,24 @@ package br.com.devmedia.construtores;
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Livro livro = new Livro();
+
+		livro.setTitulo("Java I");
+		livro.setAutor("Luciano Brito");
+		livro.setPaginas(50);
+		livro.setLancamento(false);
+
+		System.out.println(livro.toString());
+
+		Livro livro2 = new Livro("Java II");
+
+		System.out.println(livro2.toString());
+
+		Livro livro3 = new Livro(50);
+		System.out.println(livro3.toString());
+
+		Livro livro4 = new Livro("Java III", "Beltrano", 58, true);
+		System.out.println(livro4.toString());
 
 	}
 
