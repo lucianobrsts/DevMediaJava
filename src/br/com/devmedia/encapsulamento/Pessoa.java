c8a2b35d47600f9fc7c6f88e3a0561e419536ce0
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-04-14 16:14:50 -0300

    Finalizado Projeto DevMedia Encapsulamento

--- src/br/com/devmedia/encapsulamento/Pessoa.java
+++ src/br/com/devmedia/encapsulamento/Pessoa.java
@@ -1,8 +1,28 @@
 package br.com.devmedia.encapsulamento;
 
 public class Pessoa {
-	
-	String nome;
-	int idade;
+
+	private String nome;
+	private int idade;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome.toUpperCase();
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		if(idade >= 0) {
+			this.idade = idade;
+		} else {
+			this.idade = 0;
+		}
+	}
 
 }
--- src/br/com/devmedia/encapsulamento/Programa.java
+++ src/br/com/devmedia/encapsulamento/Programa.java
@@ -2,12 +2,29 @@ package br.com.devmedia.encapsulamento;
 
 public class Programa {
 
+	private boolean ativo;
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
 	public static void main(String[] args) {
 		Pessoa pessoa = new Pessoa();
-		pessoa.nome = "Maria";
-		pessoa.idade = 20;
+		pessoa.setNome("Maria");
+		pessoa.setIdade(20);
 
-		System.out.println("Pessoa: " + pessoa.nome + "\nIdade: " + pessoa.idade);
+		System.out.println("Pessoa: " + pessoa.getNome() + "\nIdade: " + pessoa.getIdade());
+		System.out.println();
+
+		Pessoa pessoa2 = new Pessoa();
+		pessoa2.setNome("Luciana");
+		pessoa2.setIdade(-8);
+
+		System.out.println("Pessoa: " + pessoa2.getNome() + "\nIdade: " + pessoa2.getIdade());
 	}
 
 }
