96dcbe26a32ecd190c95e1c77cbee5beb9cefce7
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-04-15 21:17:33 -0300

    Inicializado Projeto DevMedia Constantes

---
+++ src/br/com/devmedia/constantes/Aula.java
@@ -0,0 +1,32 @@
+package br.com.devmedia.constantes;
+
+public class Aula {
+
+	public static final String OURO = "ouro";
+	public static final String PAUS = "paus";
+	public static final String ESPADAS = "espadas";
+	public static final String COPAS = "copas";
+
+	public static void main(String[] args) {
+		String naipe = Aula.PAUS;
+
+		switch (naipe) {
+		case Aula.OURO:
+			System.out.println("Sua carta é de ouro.");
+			break;
+		case Aula.PAUS:
+			System.out.println("Sua carta é de paus.");
+			break;
+		case Aula.ESPADAS:
+			System.out.println("Sua carta é de espadas.");
+			break;
+		case Aula.COPAS:
+			System.out.println("Sua carta é de copas.");
+			break;
+		default:
+			System.out.println("Nenhum naipe está presente.");
+			break;
+		}
+	}
+
+}
