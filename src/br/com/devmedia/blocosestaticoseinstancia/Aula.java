7749cb1b531372aa05ec91ac9fa6242b2ae48d41
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-05-08 01:00:02 -0300

    Finalizado Projeto DevMedia Blocos Estáticos e Blocos de Instância

--- src/br/com/devmedia/blocosestaticoseinstancia/Aula.java
+++ src/br/com/devmedia/blocosestaticoseinstancia/Aula.java
@@ -2,4 +2,32 @@ package br.com.devmedia.blocosestaticoseinstancia;
 
 public class Aula {
 
+	static int vStatic = 1;
+
+	int vInstance = 1;
+
+	static {
+		System.out.println("bloco estático.");
+		System.out.println(vStatic);
+		System.out.println(new Aula().vInstance);
+	}
+
+	{
+		System.out.println("Bloco de Instância");
+		System.out.println(vStatic + vInstance);
+		vStatic = 8;
+	}
+
+	public Aula() {
+		super();
+		System.out.println("new Aula()");
+		this.vInstance = 9;
+	}
+
+	public static void main(String[] args) {
+		Aula a = new Aula();
+		a.vInstance = 5;
+		System.out.println(a.vInstance);
+	}
+
 }
