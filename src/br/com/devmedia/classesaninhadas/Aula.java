9bf3394e462df7cf48fe2d4270fcc179e3b732bc
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-05-08 22:34:38 -0300

    Finalizado Projeto DevMedia Classes aninhadas ou classes internas

--- src/br/com/devmedia/classesaninhadas/Aula.java
+++ src/br/com/devmedia/classesaninhadas/Aula.java
@@ -1,5 +1,19 @@
 package br.com.devmedia.classesaninhadas;
 
+import java.time.LocalDate;
+
 public class Aula {
+
+	public static void main(String[] args) {
+
+		Pessoa p = new Pessoa();
+		p.setNome("Ana");
+		p.setSobrenome("Pinheiro");
+		p.setDtNascimento(LocalDate.of(1975, 7, 20));
+
+		System.out.printf("%s %s Possui %d anos, %d meses e %d dias.", p.getNome(), p.getSobrenome(),
+				p.getIdade().getAnos(), p.getIdade().getMeses(), p.getIdade().getDias());
+
+	}
 
 }
---
+++ src/br/com/devmedia/classesaninhadas/CalculaIdade.java
@@ -0,0 +1,9 @@
+package br.com.devmedia.classesaninhadas;
+
+public interface CalculaIdade {
+	int getAnos();
+
+	int getMeses();
+
+	int getDias();
+}
