4df86f074cd6a656844be23c41028254b0846257
lucianobrsts <lucianobrsts@yahoo.com.br>
2018-05-09 11:37:49 -0300

    Finalizado Projeto DevMedia Classe interna Estática e método local

--- src/br/com/devmedia/classesaninhadas/Aula.java
+++ src/br/com/devmedia/classesaninhadas/Aula.java
@@ -1,19 +1,55 @@
 package br.com.devmedia.classesaninhadas;
 
-import java.time.LocalDate;
-
 public class Aula {
 
 	public static void main(String[] args) {
 
-		Pessoa p = new Pessoa();
-		p.setNome("Ana");
-		p.setSobrenome("Pinheiro");
-		p.setDtNascimento(LocalDate.of(1975, 7, 20));
+		// Pessoa p = new Pessoa();
+		// p.setNome("Ana");
+		// p.setSobrenome("Pinheiro");
+		// p.setDtNascimento(LocalDate.of(1975, 7, 20));
+		//
+		// System.out.printf("%s %s Possui %d anos, %d meses e %d dias.", p.getNome(),
+		// p.getSobrenome(),
+		// p.getIdade().getAnos(), p.getIdade().getMeses(), p.getIdade().getDias());
 
-		System.out.printf("%s %s Possui %d anos, %d meses e %d dias.", p.getNome(), p.getSobrenome(),
-				p.getIdade().getAnos(), p.getIdade().getMeses(), p.getIdade().getDias());
+		System.out.println("Multiplicação = " + new Aula().getCalculo(9, 5));
 
 	}
 
+	int getCalculo(int x, int y) {
+
+		System.out.println("Valor de X = " + x);
+
+		System.out.println("Valor de Y = " + y);
+		
+		int z = 10;
+
+		class Calculo {
+
+			private int a;
+			private int b;
+
+			Calculo() {
+
+			}
+
+			Calculo(int a, int b) {
+				this.a = a;
+				this.b = b;
+			}
+
+			int soma() {
+				return x + y;
+			}
+
+			int multiplicar() {
+				return a * b * z;
+			}
+		}
+
+		System.out.println("Soma = " + new Calculo().soma());
+		
+		return new Calculo(10, 2).multiplicar();
+	}
 }
